123f83415680b53314b9538eaf870599a9e3e5ca
Vikash Kumar <88263645+Vikash620404@users.noreply.github.com>
2022-07-18 20:30:08 +0530

    Insert At between  the doubly Linkedlist in java

---
+++ Insert At between  the doubly Linkedlist.java
@@ -0,0 +1,17 @@
+public Node insertAtPos(Node head,int pos,Node newNode){
+Node temp=head;
+int count=1;
+while(count<pos-1){
+temp=temp.next;
+count++;
+}
+newNode .prev=temp;
+newNode.next=temp.next;
+newNode.next.prev=newNode;
+return head;
+}
+
+
+
+
+//O(N)
